00184425dc844dfec62d4f5f9cec631e8d34d1bd
SebastianEger <eger.sebastian@gmx.de>
2015-12-11 11:14:25 +0100

    merged simulation with tlp

---
+++ tas_local_planner/src/tas_local_planner.cpp
@@ -0,0 +1,128 @@
+#include <pluginlib/class_list_macros.h>
+#include "tas_local_planner.h"
+#include <tf/transform_datatypes.h>
+
+//register this planner as a BaseGlobalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(tas_local_planner::LocalPlanner, nav_core::BaseLocalPlanner)
+
+using namespace std;
+
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
+    tlpLaserScan = scan;
+}
+
+float calcDistance(geometry_msgs::PoseStamped& a, geometry_msgs::PoseStamped& b) {
+    float xDiff = a.pose.position.x - b.pose.position.x;
+    float yDiff = a.pose.position.y - b.pose.position.y;
+    return sqrt(pow(xDiff,2) + pow(yDiff,2));
+}
+
+//Default Constructor
+namespace tas_local_planner {
+
+LocalPlanner::LocalPlanner (){
+}
+
+LocalPlanner::LocalPlanner(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros){
+    initialize(name, tf, costmap_ros);
+}
+
+void LocalPlanner::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros){
+    if(!initialized_) {
+        subScan_ = nodeHandle_.subscribe("scan", 1000, scanCallback);
+        tf_ = tf;
+        costmap_ros_ = costmap_ros_;
+        goalIsReached_ = false;
+
+        //finish initialization
+        ROS_INFO("TAS LocalPlanner succesfully initialized!");
+        initialized_ = true;
+    } else {
+        ROS_INFO("TAS LocalPlanner is already initialized!");
+    }
+}
+
+bool LocalPlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel) {
+
+    //transform robot pose
+    tf::Stamped<tf::Pose> tempRobotPose;
+    costmap_ros_->getRobotPose(tempRobotPose);
+    tf::poseStampedTFToMsg(tempRobotPose, robotPose_);
+    //--
+
+    //calc which path point is closest to current robot pose
+    float lowestDist;
+    int counter = 1;
+    int nearestPathPoint = 0;
+    lowestDist = calcDistance(robotPose_, *(plan_.begin()));
+    for(std::vector<geometry_msgs::PoseStamped>::iterator it = plan_.begin()+1; it != plan_.end(); ++it) {
+        if(calcDistance(robotPose_, *it) < lowestDist) {
+            nearestPathPoint = counter;
+            lowestDist = calcDistance(robotPose_, *it);
+        }
+        counter ++;
+    }
+    //--
+    ROS_INFO("Nearest path point: %i", nearestPathPoint);
+    ROS_INFO("Distance: %f", lowestDist);
+
+
+    //ROS_INFO("Range 320: %f", tlpLaserScan->ranges[320]);
+
+    // emergency stop
+    bool stopCar = false;
+    for(int i = 0; i < 620; i++) { //620? passt des?
+        if(tlpLaserScan->ranges[i] < 0.1) {
+            stopCar = true;
+        }
+    }
+    if(stopCar) {
+        cmd_vel.linear.x = 0;
+        return true;
+    }
+}
+bool LocalPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& plan) {
+    ROS_INFO("TLP: new global plan received!");
+    plan_ = plan;
+    return true;
+}
+bool LocalPlanner::isGoalReached() {
+    return goalIsReached_;
+}
+
+void LocalPlanner::analyzeLaserData()
+{
+    //transform to vector form
+    std::vector<geometry_msgs::Point> tmpLaserData;
+    for(int i = 0; i < 620; i++) { //620? passt des?
+        geometry_msgs::Point laserPoint;
+        laserPoint.x = sin(-1.4 + (2.8*i)/620)*tlpLaserScan->ranges[i];
+        laserPoint.y = cos(-1.4 + (2.8*i)/620)*tlpLaserScan->ranges[i];
+        tmpLaserData.push_back(laserPoint);
+    }
+    //calc direction
+    std::vector<geometry_msgs::Point> tmpLaserDirection;
+    for(std::vector<geometry_msgs::Point>::iterator it = tmpLaserData.begin()+1; it!=tmpLaserData.end(); it++) {
+        geometry_msgs::Point laserPointDirection;
+        laserPointDirection.x = it->x - (it-1)->x;
+        laserPointDirection.y = it->y - (it-1)->y;
+        tmpLaserDirection.push_back(laserPointDirection);
+    }
+
+    //retrieve objects
+    laserObjects_.clear();
+    LaserObject newObject;
+    newObject.start = *(tmpLaserData.begin());
+    int helper = 1;
+    for(std::vector<geometry_msgs::Point>::iterator it = tmpLaserDirection.begin()+1; it!=tmpLaserDirection.end(); it++) {
+        //if angle between two directions is greater than ? start new Object
+        if(atan2(it->x, it->y) - atan2((it-1)->x,(it-1)->y) > 10) {
+            newObject.end = tmpLaserData.at(helper);
+            laserObjects_.push_back(newObject);
+            newObject.start = tmpLaserData.at(helper);
+        }
+        helper ++;
+    }
+}
+
+};
---
+++ tas_local_planner/src/tas_local_planner.h
@@ -0,0 +1,66 @@
+ #include <ros/ros.h>
+ #include <costmap_2d/costmap_2d_ros.h>
+ #include <costmap_2d/costmap_2d.h>
+ #include <nav_core/base_local_planner.h>
+ #include <geometry_msgs/PoseStamped.h>
+ #include <angles/angles.h>
+ #include <base_local_planner/world_model.h>
+ #include <base_local_planner/costmap_model.h>
+ #include <sensor_msgs/LaserScan.h>
+
+ using std::string;
+
+ #ifndef TAS_LOCAL_PLANNER_CPP
+ #define TAS_LOCAL_PLANNER_CPP
+
+sensor_msgs::LaserScan::ConstPtr tlpLaserScan;
+
+enum LaserObjectType {
+    wall = 0,
+    obstacle = 1
+};
+
+struct LaserObject {
+    LaserObjectType type;
+    geometry_msgs::Point start;
+    geometry_msgs::Point end;
+};
+
+ namespace tas_local_planner {
+
+ class LocalPlanner : public nav_core::BaseLocalPlanner
+ {
+ public:
+
+  LocalPlanner();
+  LocalPlanner(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros);
+
+  /** overridden classes from interface nav_core::BaseLocalPlanner **/
+  void initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros);
+  bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+  bool setPlan(const std::vector<geometry_msgs::PoseStamped>& plan);
+  bool isGoalReached();
+
+private:
+
+  //variables
+  bool goalIsReached_;
+  bool initialized_;
+  tf::TransformListener* tf_;
+  costmap_2d::Costmap2DROS* costmap_ros_;
+  ros::NodeHandle nodeHandle_;
+  ros::Subscriber subScan_;
+  std::vector<geometry_msgs::PoseStamped> plan_;
+  geometry_msgs::PoseStamped robotPose_;
+  std::vector<geometry_msgs::Point> laserDataTf_;
+  std::vector<LaserObject> laserObjects_;
+
+
+  // functions
+  //void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+  int makeDecision();
+  void analyzeLaserData();
+
+ };
+};
+#endif
