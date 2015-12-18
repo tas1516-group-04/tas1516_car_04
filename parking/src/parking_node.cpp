92186e6533f7e269eff43d0cbf23ec5f08497da7
Thomas Eiband <thomas.eiband@gmail.com>
2015-12-18 14:33:52 +0100

    add debug functions for laser scan

--- parking/src/parking_node.cpp
+++ parking/src/parking_node.cpp
@@ -5,25 +5,33 @@
 #include "ScanFeatures.h"
 #include "../../parking_control/src/control/control.h"      // for speed defines ...
 #include <tf/transform_listener.h>
+#include <iostream>
 
+using namespace std;
 
-#define MAX_DIST            0.35
-#define MIN_DIST            0.3
-#define NUM_MEAN_SAMPLES    10
+#define MAX_DIST                0.35
+#define MIN_DIST                0.3
+#define NUM_MEAN_SAMPLES        10
 
-#define MIN_GAP_DEPTH       0.2
-#define MAX_GAP_DEPTH       0.35
+#define MIN_GAP_DEPTH           0.2
+#define MAX_GAP_DEPTH           0.35
 
-#define MIN_GAP_LENGTH      0.4
-#define MAX_GAP_LENGTH      1.0
+#define MIN_GAP_LENGTH          0.4
+#define MAX_GAP_LENGTH          1.0
 
-#define RANGE_THRESHOLD     0.05
+#define RANGE_THRESHOLD         0.05
+
+#define BACKWARD_THRESHOLD_1    0.5
+#define BACKWARD_THRESHOLD_2    0.1
 
 
 typedef struct {
     float new_dist;
     float old_dist;
     float diff_dist;
+    float left_dist;
+    float right_dist;
+    float middle_dist;
 } dist_t;
 
 typedef struct {
@@ -55,17 +63,23 @@ typedef struct {
 } corners_t;
 
 
-float yposF = 9999;
-float yposB = 9999;
+dist_t Front;
+dist_t Back;
 
 void processLaserScanF(const sensor_msgs::LaserScan::ConstPtr& scan){
-     //scan->ranges[] are laser readings
-    yposF = scan->ranges[0];
+    //scan->ranges[] are laser readings
+    Front.left_dist = scan->ranges[0];
+    Front.middle_dist = scan->ranges[scan->ranges.size() / 2];
+    Front.right_dist = scan->ranges[scan->ranges.size() - 1];
+    // DEBUG
+    cout << "* Front:" << endl << "  left: " << Front.left_dist << " middle: " << Front.middle_dist << " right: " << endl;
 }
 
 void processLaserScanB(const sensor_msgs::LaserScan::ConstPtr& scan){
-     //scan->ranges[] are laser readings
-    yposB = scan->ranges[scan->ranges.size() - 1];
+    //scan->ranges[] are laser readings
+    Front.left_dist = scan->ranges[0];
+    Front.middle_dist = scan->ranges[scan->ranges.size() / 2];
+    Front.right_dist = scan->ranges[scan->ranges.size() - 1];
 }
 
 int main(int argc, char** argv)
@@ -94,12 +108,13 @@ int main(int argc, char** argv)
                  FIRST_CORNER_END,
                  SECOND_CORNER_START,
                  SECOND_CORNER_END,
-                 START_PARKING };
+                 START_PARKING,
+                 PARKING_STATE_1,
+                 PARKING_STATE_2,
+                 END_PARKING };
 
     int state = INIT;
 
-    dist_t Front;
-    dist_t Back;
     robot_t R;
     corners_t C;
 
@@ -112,7 +127,7 @@ int main(int argc, char** argv)
     {
         geometry_msgs::Twist vel_msg;
         vel_msg.linear.x = PARKING_SPEED;
-        vel_msg.angular.z = STEERING_RATIO;
+        vel_msg.angular.z = 0;
 
 
 
@@ -130,6 +145,7 @@ int main(int argc, char** argv)
         R.ypos = transform.getOrigin().y();
         */
 
+        /*
         // Calc distances for front laser scan, very LEFT ray
         Front.new_dist = yposF;
         Front.diff_dist = Front.old_dist - Front.new_dist;
@@ -139,6 +155,7 @@ int main(int argc, char** argv)
         Back.new_dist = yposB;
         Back.diff_dist = Back.old_dist - Back.new_dist;
         Back.old_dist = Back.new_dist;
+        */
 
         switch(state) {
 
@@ -172,7 +189,9 @@ int main(int argc, char** argv)
             }
             break;
         case FIRST_CORNER_END:
-
+            // steer robot
+            vel_msg.linear.x = PARKING_SPEED;
+            vel_msg.angular.z = Front.diff_dist * STEERING_RATIO;   // TODO define steering ratio
             // check if in range to detect first corner end
             if (Front.new_dist <= MIN_GAP_DEPTH) {
                 ROS_INFO("dist_diff is in range for first corner end");
@@ -185,8 +204,12 @@ int main(int argc, char** argv)
             }
             break;
         case SECOND_CORNER_START:
-            if (Front.diff_dist >= RANGE_THRESHOLD) {
-                ROS_INFO("dist_diff is in range for first corner end");
+            // steer robot
+            vel_msg.linear.x = PARKING_SPEED;
+            vel_msg.angular.z = Front.diff_dist * STEERING_RATIO;   // TODO define steering ratio
+
+            if (Back.new_dist >= MIN_GAP_DEPTH && Back.new_dist <= MAX_GAP_DEPTH) {
+                ROS_INFO("dist_diff is in range for second corner start");
 
                 // capture robot position now for second corner start
                 C.second.start.xpos = R.xpos;
@@ -195,9 +218,10 @@ int main(int argc, char** argv)
                 state = SECOND_CORNER_END;
             }
             break;
+        /*
         case SECOND_CORNER_END:
             if (Front.diff_dist >= RANGE_THRESHOLD) {
-                ROS_INFO("dist_diff is in range for first corner end");
+                ROS_INFO("dist_diff is in range for second corner end");
 
                 // capture robot position now for second corner end
                 C.second.end.xpos = R.xpos;
@@ -206,12 +230,59 @@ int main(int argc, char** argv)
                 state = START_PARKING;
             }
             break;
+        */
         case START_PARKING:
             ROS_INFO("All corners detected. Start Parking:");
-            ROS_INFO("Set max steering angle and drive backwards...");
+            ROS_INFO("Driving backwards with max steering angle...");
+
             vel_msg.linear.x = - PARKING_SPEED;
             vel_msg.angular.z = MAX_STEERING;
 
+            // check if first backward distance reached
+            if (Back.new_dist <= BACKWARD_THRESHOLD_1) {
+                state = PARKING_STATE_1;
+            }
+
+            break;
+
+        case PARKING_STATE_1:
+            ROS_INFO("Steering reversal point reached!");
+            ROS_INFO("Driving Backwards with min. steering angle...");
+
+            vel_msg.linear.x = - PARKING_SPEED;
+            vel_msg.angular.z = - MAX_STEERING;
+
+            // check if first backward distance reached
+            if (Back.new_dist <= BACKWARD_THRESHOLD_2) {
+                state = PARKING_STATE_2;
+            }
+
+            break;
+
+        case PARKING_STATE_2:
+            ROS_INFO("Minimum distance to back reached!");
+            ROS_INFO("Driving forward for 0.1 m if way is cleared");
+
+            // TODO add ray component for middle ray here
+            if (Front.new_dist >= 0.2) {
+                vel_msg.linear.x = PARKING_SPEED;
+                vel_msg.angular.z = 0;
+            }
+
+            // check if forward min distance reached
+            if (Front.new_dist <= 0.2) {
+                state = END_PARKING;
+            }
+
+            break;
+
+        case END_PARKING:
+            ROS_INFO("Parking Finished. Setting speed to zero!");
+            vel_msg.linear.x = PARKING_SPEED;
+            vel_msg.angular.z = 0;
+
+            ROS_INFO("Finished! Exiting...");
+            exit(EXIT_SUCCESS);
             break;
 
          default:
